631dc8f9ac9fa22b37ec2679ced6f746faf1d484
Sabri Eyuboglu <eyuboglu@stanford.edu>
2021-07-28 13:49:08 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+REQUIRED = [
+    "pre-commit",
+    "pytorch-lightning",
+    "pandas",
+    "numpy>=1.18.0",
+    "cytoolz",
+    "ujson",
+    "jsonlines>=1.2.0",
+    "torch>=1.8.0",
+    "tqdm>=4.49.0",
+    "scikit-learn",
+    "torchvision>=0.9.0",
+    "wandb",
+    "ray[default]",
+    "torchxrayvision",
+]
+
+setuptools.setup(
+    name="dcbench",
+    version="0.0.1",
+    description="Data-centric AI benchmark.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HazyResearch/data-centric-benchmark",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=REQUIRED,
+)
