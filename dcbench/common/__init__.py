d5d33fd69b4757f9dd1d06321e094e1fb1e99901
Sabri Eyuboglu <eyuboglu@stanford.edu>
2021-11-19 16:45:38 -0800

    Fix import issue

--- dcbench/common/__init__.py
+++ dcbench/common/__init__.py
@@ -1,1 +1,10 @@
 # flake8: noqa
+
+from .artifact import Artifact
+from .problem import Problem
+from .result import Result
+from .solution import Solution
+from .table import Table
+from .task import Task
+
+__all__ = ["Artifact", "Problem", "Solution", "Task", "Table", "Result"]
